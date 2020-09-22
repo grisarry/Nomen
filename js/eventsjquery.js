9256b96eaa250a387098594fe379a88bc36736a4
Gri <69758002+grisarry@users.noreply.github.com>
2020-09-21 22:45:08 -0300

    Add files via upload

--- js/eventsjquery.js
+++ js/eventsjquery.js
@@ -1,6 +1,23 @@
-	$(document).ready(function()
-	    {
+	$(document).ready(function(){
+
+
 	    $("#cartIcon").on( "click", function() {	 
-	        $('.sideNavCart').toggle();
-	         });
-	    });
+			$('.sideNavCart').animate({
+				  width: 'toggle'
+			});
+		});
+
+		$(".closebtn").on( "click", function() {	 
+			$('.sideNavCart').animate({
+				  width: 'toggle'
+			});
+		});
+
+
+
+	});
+
+
+
+
+
