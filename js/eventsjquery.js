f28e70368a04cb23ed9cdcfc3e0a006dacdf251d
Gri <69758002+grisarry@users.noreply.github.com>
2020-09-28 23:08:02 -0300

    Add files via upload

--- js/eventsjquery.js
+++ js/eventsjquery.js
@@ -1,23 +1,36 @@
 	$(document).ready(function(){
 
-
 	    $("#cartIcon").on( "click", function() {	 
-			$('.sideNavCart').animate({
-				  width: 'toggle'
-			});
+			$('.sideNavCart').toggleClass( "openCart" , 1000);
+				$('.cartMask').toggleClass( "openCart" , 1000);
+		});
+		
+		$(".closebtn").on( "click", function() {	 
+			$('.sideNavCart').toggleClass( "openCart" , 1000);
+				$('.cartMask').toggleClass( "openCart" , 1000);
 		});
 
-		$(".closebtn").on( "click", function() {	 
-			$('.sideNavCart').animate({
-				  width: 'toggle'
-			});
+		$(".cartMask").on( "click", function() {	 
+				$('.cartMask').toggleClass( "openCart" , 1000);
+					$('.sideNavCart').toggleClass( "openCart" , 1000);
 		});
+
+
+
+
+
+		//AJAX alert producto agregado al carrito
+			$(".titulo-pagina-productos").click(function(){
+				$.get( "prueba_post.php",
+				function(data, status){
+					  alert("Resultado: " + data);
+			  			// La respuesta del llamado es el contenido JSON generado por el archivo PHP
+				});
+			  });
+			  
+		
 
 
 
 	});
-
-
-
-
 
